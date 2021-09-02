abf6dc20bde87dc5e49fbadcf36a68d544faa401
reynir <reynirog20@ru.is>
2021-09-02 15:46:24 +0000

    Eftir aðstoð samnemanda

--- sequence.py
+++ sequence.py
@@ -1,6 +1,16 @@
 n = int(input("Enter the length of the sequence: ")) 
-sum = 1
 
+first_num = 1
+second_num = 2
+third_num = 3
+
+print(first_num)
+print(second_num)
+print(third_num)
 for i in range(n):
-    sum = sum + i
-    print(sum)
+    if i >= 3:
+        sum_num = first_num + second_num + third_num
+        print(sum_num)
+        first_num = second_num
+        second_num = third_num
+        third_num = sum_num
